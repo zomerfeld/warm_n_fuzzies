39d975407b532813ca3faf13b079cd8c4b95ba58
zomerfeld <zomerfeld@gmail.com>
2017-04-25 20:29:22 -0700

    No freezing of the code

--- src/warm.ino
+++ src/warm.ino
@@ -1,6 +1,6 @@
 // This #include statement was automatically added by the Particle IDE.
 #include <Adafruit_LSM303_U.h>
-#include <Particle-GPS.h> 
+#include <Particle-GPS.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
 
@@ -36,26 +36,26 @@ int target(String command); //function that gives the target location
 
 
 //**** PORJECT VARIABLES ****
-int heaters[]={A0,A1,A2,2,3,4,5,6,}; //list all of the heaters pins in an array. If you add or change a heater pin, add it to here. 
+int heaters[]={A0,A1,A2,2,3,4,5,6,}; //list all of the heaters pins in an array. If you add or change a heater pin, add it to here.
 int onOff; //saves the command for the heaters as an integer
-int tHeat = 1; //toggle for heating - if this is set to 0 the heaters will not go on. 
+int tHeat = 1; //toggle for heating - if this is set to 0 the heaters will not go on.
 int coldPin = A5; //a pin that lights up an LED when all heating elements are off
 unsigned long bootupTime;
 unsigned long lastGPStime = 0;
-String heater; // what heater is supposed to be 
+String heater; // what heater is supposed to be
 String location; //current public location
 
-float lat; //Latitude 
-float lon; //longitute 
-double latPublic; //Latitude casted for variable publishing 
-double lonPublic; //Longitude casted for variable publishing 
+float lat; //Latitude
+float lon; //longitute
+double latPublic; //Latitude casted for variable publishing
+double lonPublic; //Longitude casted for variable publishing
 String oldLoc; // Stores the previous target location
 float targetLat; //The Latitude for the target location
 float targetLon; //The Longitude for the target location
 
 double compassPublic; //Compass heading
-String directionality = "xx"; //The expression of cardinal direction (NE/SW) from GPS location to target 
-double bearing; //the compass angle calculated between location and target 
+String directionality = "xx"; //The expression of cardinal direction (NE/SW) from GPS location to target
+double bearing; //the compass angle calculated between location and target
 // float magxOffset = 2.55; //offsets if needed
 // float magyOffset = 27.95;
 float magxOffset = 0;
@@ -65,14 +65,14 @@ float magyOffset = 0;
 // The RX an TX pins are connected to the TX and RX pins on the device (Serial1).
 Gps _gps = Gps(&Serial1);
 
-// **** CREATE TIMERS *** 
+// **** CREATE TIMERS ***
 Timer _timer = Timer(1, onSerialData); // This timer will fire every 1 ms to receive data from the GPS (you can't change that #)
 Timer _timerCompass = Timer(500, getCompData); // This timer will fire every 0.5 seconds to capture Compass data
 Timer _timerWear = Timer(1000, wearable); //This timer will start the entire sequence.
 
 
 void setup() {
-    
+
     // **** CLOUD VARIABLES ***
     Particle.variable("Compass", compassPublic);
     Particle.variable("Latitude", latPublic);
@@ -86,33 +86,33 @@ void setup() {
 
     // **** OTHER VARIABLES ****
     pinMode(coldPin, OUTPUT); // set the ColdPin LED to be output
-  
+
     //turn off all heaters
       for (int i=0; i < sizeof(heaters); i++) {
         pinMode(heaters[i],OUTPUT);
         digitalWrite(heaters[i],LOW);
       }
-  
+
     Particle.publish("All Heaters","OFF", 31536000, PRIVATE);
 
-// **** DECLARES FUNCTIONS TO PARTICLE CLOUD 
+// **** DECLARES FUNCTIONS TO PARTICLE CLOUD
     Particle.function("toggleHeat", toggleHeat); //declares the Heat Toggle function
     Particle.function("heat", heat); //declares the HEAT cloud function
     Particle.function("GetGPS", getGPS); //declares the GPS cloud function
     Particle.function("SetTarget", setTarget); //declares the Target Setting function
     Particle.function("AntennaStat", antStat); //declares the Antenna cloud function
     Particle.function("GetCompass", getComp); //declares the Compass cloud function
-  
+
 
     delay(4000); // wait 4 seconds for you to start a Serial monitor if needed
-  
+
     Serial.begin(); //starts USB serial, defaults to 9600 BPS
     Serial.println("Initializing...");
 
   // **** Initialize the GPS *****
     _gps.begin(9600); //this is for Serial for the GPS, wired to serial1 - TX and RX on the photon
-  
-  // **** START TIMERS **** 
+
+  // **** START TIMERS ****
     bootupTime = millis();
     _timer.start();
     _timerCompass.start();
@@ -128,12 +128,12 @@ void setup() {
     /* There was a problem detecting the LSM303 ... check your connections */
     Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
     Particle.publish("Compass: ","Ooops, no LSM303 detected ... Check your wiring!",31536000, PRIVATE); //publish the command
-    while(1); // will freeze machine
+    /*while(1); // will freeze machine if no LSM is detected*/
    }
   /* Display some basic information on this compass */
    displaySensorDetails();
    setTarget("21.422474, 39.826192"); //starts with a default destination - Mecca
-  
+
 }
 
 
@@ -145,7 +145,7 @@ void setup() {
 
 void loop() {
 
-    unsigned long currentMillis = millis(); 
+    unsigned long currentMillis = millis();
         if ((currentMillis - bootupTime >= 8000) && (currentMillis - lastGPStime >= 1000)) { //waits 8 seconds from bootup
             getGPS("1"); //Gets GPS
             lastGPStime = millis();
@@ -162,7 +162,7 @@ void wearable() { //function to run every second and update the entire setup
     if (currentMillis - bootupTime >= 10000) { //waits 10 seconds from bootup
         getDirection(); // gets the cardinal directionality
         getBearing(); //gets the bearing in degrees
-        heatBearing(); //turns on the relveant heater    
+        heatBearing(); //turns on the relveant heater
         }
 }
 // *************** WEARABLE SEQUENCE ***************
@@ -175,18 +175,18 @@ void onSerialData() { //gets the GPS DATA, runs ever 1 ms
 // *************** COMPASS DATA ***************
 // THERE ARE TWO FUNCTIONS FOR THIS, one triggered automatically (getCompData) which updates a cloud variable but doesn't publish an event, and another one (getComp) that does both
 
-void getCompData() {  // *** DOESN'T PUBLISH 
+void getCompData() {  // *** DOESN'T PUBLISH
   /* Get a new compass event */
   sensors_event_t event;
   mag.getEvent(&event);
-  
+
   // Hold the module so that Z is pointing 'up' and you can measure the heading with x&y
   // Calculate heading when the magnetometer is level, then correct for signs of axis.
-  // to Change orientation (without accelerometer), change the axis below to the one *not* facing up. 
+  // to Change orientation (without accelerometer), change the axis below to the one *not* facing up.
 
   float heading = (atan2(event.magnetic.y + magyOffset,event.magnetic.x + magxOffset) * 180) / M_PI;
 
-// Normalize to 0-360 
+// Normalize to 0-360
   if (heading < 0)
   {
     heading = 360 + heading;
@@ -212,21 +212,21 @@ int getComp(String command) { // DOES PUBLISH
 
   // Hold the module so that Z is pointing 'up' and you can measure the heading with x&y
   // Calculate heading when the magnetometer is level, then correct for signs of axis.
-  // to Change orientation (without accelerometer), changed the axis below to the one *not* facing up - X in my case. 
+  // to Change orientation (without accelerometer), changed the axis below to the one *not* facing up - X in my case.
   //   float heading = atan2(event.magnetic.y, event.magnetic.x);
   float heading = (atan2(event.magnetic.y + magyOffset,event.magnetic.x + magxOffset) * 180) / M_PI;
-  
+
   // Normalize to 0-360
   if (heading < 0) {
     heading = 360 + heading;
   }
-  
+
   compassPublic = (double)heading;
 
   // *** PUBLISH HEADING - DEGREES ***
   Serial.print("Heading (degrees): "); Serial.println(compassPublic);
   Particle.publish("CompassHeading: ", String(compassPublic), 31536000, PRIVATE); //publishes the compass heading
-  
+
   return 1;
 
 }
@@ -237,20 +237,20 @@ int getComp(String command) { // DOES PUBLISH
 int toggleHeat(String Command) {
     tHeat = Command.toInt(); //casts the string command to an int
     if (tHeat == 1) {
-        Particle.publish("HeatToggle","ON",31536000, PRIVATE); 
+        Particle.publish("HeatToggle","ON",31536000, PRIVATE);
 
     } else {
         heat("0");
-        Particle.publish("HeatToggle","OFF",31536000, PRIVATE); 
+        Particle.publish("HeatToggle","OFF",31536000, PRIVATE);
     }
 }
 
 // **** TURNS ON HEAT PADS BY COMMAND ****
-int heat(String command) { 
-    
+int heat(String command) {
+
     // Particle.publish("command was ",command,31536000, PRIVATE); //publish what heater is supposed to turn on
     heater = command;
-    
+
     if (tHeat != 1) { //don't heat anything if toggle is set to 0
         //turn off all heaters
          for (int i=0; i<sizeof(heaters); i++) {
@@ -266,7 +266,7 @@ int heat(String command) {
         digitalWrite(A0, HIGH);
         digitalWrite(coldPin, LOW);
         // Particle.publish("Heater on:",command, 31536000, PRIVATE); //DEBUG
-    
+
     } else if (command == "A1") { //skips verifications because A1 isn't an INT
     //turn off all heaters
     for (int i=0; i<sizeof(heaters); i++) {
@@ -275,7 +275,7 @@ int heat(String command) {
         digitalWrite(A1, HIGH);
         digitalWrite(coldPin, LOW);
         // Particle.publish("Heater on:",command, 31536000, PRIVATE); //DEBUG
-    
+
     } else if (command == "A2") { //skips verifications because A2 isn't an INT
     //turn off all heaters
     for (int i=0; i<sizeof(heaters); i++) {
@@ -284,11 +284,11 @@ int heat(String command) {
         digitalWrite(A2, HIGH);
         digitalWrite(coldPin, LOW);
         // Particle.publish("Heater on:",command, 31536000, PRIVATE); //DEBUG
-    
-    } else { 
-      // *** This function is supposed to verify if the command is in the array. if it's not, turn on the off pin 
+
+    } else {
+      // *** This function is supposed to verify if the command is in the array. if it's not, turn on the off pin
       onOff = command.toInt(); // casts the command to an INT
-     
+
       if (onOff == 0) { // 0 works as a default off to all heaters
         //turn off all heaters
         for (int i=0; i<sizeof(heaters); i++) {
@@ -296,16 +296,16 @@ int heat(String command) {
         }
         digitalWrite(coldPin, HIGH);
       }
-      
+
       int wasFound = searchArray(onOff); //looks in the array for the number given in the command
     //   Particle.publish("HeaterFoundInArray?", String(wasFound), 31536000, PRIVATE); // DEBUG
-      
-      if (wasFound == 1) { //if found in heaters array - this doesn't quite work accurately 
+
+      if (wasFound == 1) { //if found in heaters array - this doesn't quite work accurately
         //turn off all heaters
         for (int i=0; i<sizeof(heaters); i++) {
           digitalWrite(heaters[i],LOW);
         }
-        
+
         if (onOff !=0) { // if onOff is an integer (heater number) and not zero
             digitalWrite(onOff, HIGH); //turns on the Specific heater
             // Particle.publish("Heater on:",command, 31536000, PRIVATE); // DEBUG - which heater is on
@@ -318,12 +318,12 @@ int heat(String command) {
         Particle.publish("Invalid command - Not in array",command, 31536000, PRIVATE);
       }
     }
-    
+
       return 1; //ends the function
     }
-    
-// **** FUNCTION TO SEARCH ARRAY    
-int searchArray (int cmd) { 
+
+// **** FUNCTION TO SEARCH ARRAY
+int searchArray (int cmd) {
     int found = 0;
     for (int arrayI = 0; arrayI < sizeof(heaters); arrayI++) {
         if (heaters[arrayI] == cmd) {
@@ -341,7 +341,7 @@ int searchArray (int cmd) {
 // *************** GPS FUNCTIONS ***************
 
 // **** GETS AND PUBLISHES GPS ANTENNA STATUS  ****
-int antStat(String command) { 
+int antStat(String command) {
   // *** Get the Antenna Status ($PGTOP).
   Pgtop pgtop = Pgtop(_gps);
   if (pgtop.parse()) {
@@ -381,7 +381,7 @@ int getGPS(String command) { //publishes and triggers sub functions for bearing 
     // Serial.print("East/WestIndicator: "); Serial.println(gga.eastWestIndicator);
     // Particle.publish("Lat: ", gga.latitude + gga.northSouthIndicator, 31536000, PRIVATE);
     // Particle.publish("Lon: ", gga.longitude + gga.eastWestIndicator, 31536000, PRIVATE);
-    
+
     double latN = convertDegMinToDecDeg(gga.latitude.toFloat()); //in Decimal Degrees
     //.toFloat() converts the string from the GPS to for the convert function to work
     if (String(gga.eastWestIndicator) == "N") {
@@ -391,10 +391,10 @@ int getGPS(String command) { //publishes and triggers sub functions for bearing 
         if (String(gga.eastWestIndicator) == "W") {
         lonN = lonN * (-1);
     }
-    
+
     lat = latN; //save to global variables - These are floats
-    lon = lonN; 
-    
+    lon = lonN;
+
     // these are for using in cloud variables - Doubles
     latPublic = (double)lat;
     lonPublic = (double)lon;
@@ -412,7 +412,7 @@ int getGPS(String command) { //publishes and triggers sub functions for bearing 
     // Serial.print("Geoidal Separation: "); Serial.print(gga.geoidalSeparation); Serial.print(" "); Serial.println(gga.geoidalSeparationUnit);
     // Serial.print("Age of Diff. Corr.: "); Serial.println(gga.ageOfDiffCorr);
     Serial.println("");
-    
+
 
 //   Rmc rmc = Rmc(_gps); // **** GPS DIRECTION *** Not accurate
 //   if (rmc.parse())
@@ -420,9 +420,9 @@ int getGPS(String command) { //publishes and triggers sub functions for bearing 
 //     Serial.print("Course Over Ground: "); Serial.println(rmc.courseOverGround);
 //     Particle.publish("COG: ", String(rmc.courseOverGround), 31536000, PRIVATE);
   }
-  
+
   if (gga.latitude!=NULL) { // Return 0 if no GPS data is available yet
-    return 1; 
+    return 1;
   } else {
       return 0;
   }
@@ -433,14 +433,14 @@ int getGPS(String command) { //publishes and triggers sub functions for bearing 
 double convertDegMinToDecDeg (float degMin) {
   double min = 0.0;
   double decDeg = 0.0;
- 
+
   //get the minutes, fmod() requires double
   min = fmod((double)degMin, 100.0);
- 
+
   //rebuild coordinates in decimal degrees
   degMin = (int) ( degMin / 100 );
   decDeg = degMin + ( min / 60 );
- 
+
   return decDeg;
 }
 
@@ -449,7 +449,7 @@ double convertDegMinToDecDeg (float degMin) {
 int setTarget(String command) { //sets the target destination GPS, accept Lat+Lan in a string, separated by a comma or a comma+space, as given by google maps
     // example from google maps - https://screencast.com/t/3M5zccZ6w
     // 37.779098, -122.415871 (SF Public library)
-    
+
         int i = command.indexOf(','); // separator is a comma
         //LAT
         String tarLatTemp = command.substring(0,i); //splits the string to two, where the comma is
@@ -459,12 +459,12 @@ int setTarget(String command) { //sets the target destination GPS, accept Lat+La
         String tarLonTemp = command.substring(i+1);
         tarLonTemp = tarLonTemp.trim(); //removes white space
         targetLon = tarLonTemp.toFloat();
-        
+
         Particle.publish("targetLat: ", String(targetLat), 31536000, PRIVATE);
         Particle.publish("tarLon: ", String(targetLon), 31536000, PRIVATE);
         oldLoc = command; //saves the location to the old location
-        
-    
+
+
         getDirection(); // gets the cardinal directionality
         getBearing(); //gets the bearing in degrees
         heatBearing(); //turns on the relveant heater
@@ -472,7 +472,7 @@ int setTarget(String command) { //sets the target destination GPS, accept Lat+La
         return 1;
 }
 
-void getDirection() { //only gets caridnal directions, which is not very helpful 
+void getDirection() { //only gets caridnal directions, which is not very helpful
     // Norther is larger numbers
     // Easter is larger number (In negative it means less negative, obviously)
     // This probably needs extra math for past 0 points in latitude or longitude
@@ -483,7 +483,7 @@ void getDirection() { //only gets caridnal directions, which is not very helpful
     } else {
         directionality = "X"; // x marks the spot
     }
-    
+
     if (targetLon > lon) {
         directionality = directionality + "E";
     } else if (targetLon < lon) {
@@ -495,23 +495,23 @@ void getDirection() { //only gets caridnal directions, which is not very helpful
 
 
 void getBearing() { //calculates angle from location to destination based on GPS (no compass yet)
-    //startLat,startLong,endLat,endLong 
-    
+    //startLat,startLong,endLat,endLong
+
     float startLatTemp = (float)latPublic; //takes the public variable for GPS current location
-    float startLongTemp = (float)lonPublic; 
+    float startLongTemp = (float)lonPublic;
     float endLatTemp = (float)targetLat; //takes the public variable for target
     float endLongTemp = (float)targetLon;
-    
+
     //convert to Radians
-    float startLat = radians(startLatTemp); 
+    float startLat = radians(startLatTemp);
     float startLong = radians(startLongTemp);
     float endLat = radians(endLatTemp);
     float endLong = radians(endLongTemp);
-    
+
     float dLong = endLong - startLong;
-    
+
     float dPhi = log(tan(endLat/2.0+PI/4.0)/tan(startLat/2.0+PI/4.0));
-    
+
       if (abs(dLong) > PI) {
         if (dLong > 0.0) {
            dLong = -(2.0 * PI - dLong);
@@ -528,16 +528,16 @@ void getBearing() { //calculates angle from location to destination based on GPS
 // *************** USES BEARING TO CONTROL HEATING ***************
 
 void heatBearing() {
-    float heatBearing; //turns on the relevant heat pad using the heat() function 
+    float heatBearing; //turns on the relevant heat pad using the heat() function
 
     // A0 is North and then in 45 deg jumps clockwise - 2,3,4,A1(S),5,6,A2
     // account for the direction I'm facing based on the compass reading
-    if ((bearing - compassPublic) > 0) { 
+    if ((bearing - compassPublic) > 0) {
         heatBearing = bearing - compassPublic;
     } else {
         heatBearing = bearing - compassPublic +360;
     }
-    
+
     if (heatBearing == 0) { // only for Specific 0 which will appear if I'm missing a GPS reading or target
         Particle.publish("No Bearing", NULL, 31536000, PRIVATE);
         heat("n"); //turn off heaters
@@ -557,9 +557,7 @@ void heatBearing() {
         heat("6");
     } else if (heatBearing > 292.5 && heatBearing <= 337.5) {
         heat("A2");
-    } 
-    
-       
+    }
+
+
 }
-
-
